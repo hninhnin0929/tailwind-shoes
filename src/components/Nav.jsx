9c63a48197ae68d0213f931d7a2c920ac0f4df33
unknown <hninhnin0929>
2024-02-04 21:40:51 +0900

    responsive mobile menu

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,7 @@
+import { TbShoppingCart } from "react-icons/tb"; 
 import NikeLogo from "../assets/nike-logo.svg?react";
 import { RxHamburgerMenu } from "react-icons/rx";
+import { useState } from "react";
 
 const ROUTES = [
     "Home",
@@ -10,6 +12,7 @@ const ROUTES = [
 ];
 
 export function Nav() {
+    const [ isMobileMenuShown, setMobileMenuShown ] = useState(false);
   return (
     <nav className="flex flex-wrap items-center justify-between">
         {/* Logo */}
@@ -18,17 +21,19 @@ export function Nav() {
         </a>
 
         {/* Burger button */}
-        <button className="rounded-lg p-2 hover:bg-gray-100 focus:ring-2 focus:ring-gray-200">
+        <button onClick={()=> setMobileMenuShown(!isMobileMenuShown)} className="lg:hidden rounded-lg p-2 hover:bg-gray-100 focus:ring-2 focus:ring-gray-200">
             <RxHamburgerMenu size={25}/>
         </button>
 
         {/* Menu List */}
-        <div className="w-full">
-            <ul className="bg-gray-50 text-lg border border-gray-100 rounded-lg"> 
+        <div className={`${!isMobileMenuShown && "hidden" } w-full lg:block lg:w-auto`}>
+            <ul className="lg:space-x-8 flex flex-col lg:flex-row rounded-lg text-lg border border-gray-100 bg-gray-50 p-4 text-lg lg:flex-row lg:border-none lg:bg-transparent"> 
                 {
                     ROUTES.map((route, i) => {
                         return (
-                            <li className={`py-2 px-3 ${i === 0 ? "bg-blue-500 text-white" : ""}`}
+                            <li className={`py-2 px-3 ${i === 0 
+                                ? "bg-blue-500 text-white lg:bg-transparent lg:text-blue-500" 
+                                : "hover:bg-gray-100"}`}
                             key={route}>
                                 {route}
                             </li>
@@ -38,6 +43,12 @@ export function Nav() {
             </ul>
         </div>
 
+        {/* Cart Button */}
+        <div className="fixed bottom-4 left-4 lg:static">
+            <div className="flex items-center justify-center h-12 w-12 rounded-full bg-white shadow-md">
+                <TbShoppingCart />
+            </div>       
+        </div>   
     </nav>
   );
 }

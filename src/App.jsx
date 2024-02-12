aa6d0f936047f37916b4e4ca2330a093ed21588d
unknown <hninhnin0929>
2024-02-12 16:05:09 +0900

    shoe detail layout, gradient and fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 
 import './App.css'
 import { Nav } from './components/Nav';
+import { ShopDetail } from './components/ShoeDetail';
 
 export function App() {
   return <div className="p-10 xl:px-24">
     <Nav />
+    <ShopDetail />
   </div>;
 }
 
--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,6 @@
 // import React from 'react'
+// Supports weights 200-900
+import '@fontsource-variable/nunito-sans';
 import ReactDOM from 'react-dom/client'
 import { App } from './App.jsx'
 import './index.css'

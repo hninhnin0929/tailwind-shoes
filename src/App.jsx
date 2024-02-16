73427a90f59ed3632ec349c6c522b7faa083d77b
hnin <hnin0929@ecplaza.net>
2024-02-16 17:16:02 +0900

    card

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 
 import './App.css'
+import { Card } from './components/Card';
 import { Nav } from './components/Nav';
 import { ShopDetail } from './components/ShoeDetail';
+import { SHOE_LIST } from './constant';
 
 export function App() {
-  return <div className="p-10 xl:px-24">
-    <Nav />
-    <ShopDetail />
+  return <div className="p-10 xl:px-24 animate-fadeIn">
+    {/* <Nav />
+    <ShopDetail /> */}
+    <Card item={SHOE_LIST[0]} />
   </div>;
 }
 
--- src/components/ShoeDetail.jsx
+++ src/components/ShoeDetail.jsx
@@ -9,7 +9,7 @@ export function ShopDetail() {
         {/* Shoe image */}
         <div className="flex-1 lg:-mt-32 lg:ml-28 ">
             <div className="flex-center h-full bg-gradient-to-br from-[#F637CF] from-5% via-[#F3D876] via-40% to-[#4DD4C6]">
-                <img src={nike1} />
+                <img className="animate-float" src={nike1} />
             </div>
         </div>
         <div className="flex-1 space-y-6">
@@ -31,7 +31,7 @@ export function ShopDetail() {
             </div>
             {/* Shoe buttons and links */}
             <div className="space-x-10">
-                <button className="h-14 w-44 bg-black text-white hover:bg-gray-900 active:bg-gray-700">
+                <button className="h-14 w-44 bg-black text-white hover:bg-gray-900 btn-press-anim active:bg-gray-700">
                     Add to bag
                 </button>
                 <a href="#" className="text-lg font-bold underline underline-offset-4">
